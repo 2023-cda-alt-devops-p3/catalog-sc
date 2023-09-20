b6db77d008f57df5699cbff3828992b0d1769793
Criado Sébastien <dev@letgy.com>
2023-09-20 12:25:58 +0200

    feat: :sparkles: Add component page with fetch
    HTML,CSS and JS

--- app.js
+++ app.js
@@ -2,14 +2,9 @@ const sideBar = document.querySelector(".sidebar");
 const button = document.querySelector(".burger");
 const classesLink = document.getElementById("classes");
 const objectLink = document.getElementById("objects");
+const componentLink = document.getElementById("component");
 
 classesLink.addEventListener("click", () => {
-  // const mainContent = document.querySelector(".main-content");
-
-  // if (mainContent) {
-  //   mainContent.remove();
-  // }
-
   fetch("classes.html")
     .then((response) => response.text())
     .then((content) => {
@@ -19,6 +14,14 @@ classesLink.addEventListener("click", () => {
 
 objectLink.addEventListener("click", () => {
   fetch("objects.html")
+    .then((response) => response.text())
+    .then((content) => {
+      document.querySelector(".content").innerHTML = content;
+    });
+});
+
+componentLink.addEventListener("click", () => {
+  fetch("component.html")
     .then((response) => response.text())
     .then((content) => {
       document.querySelector(".content").innerHTML = content;
