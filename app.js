f805083255d92d57513ed0ec66fbb470697cff60
Criado Sébastien <dev@letgy.com>
2023-09-20 10:31:43 +0200

    fix: :bug: Fixed sidebar bug

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const sideBar = document.querySelector(".sidebar");
 const button = document.querySelector(".burger");
-const crossBtn = document.querySelector(".cross");
 
 button.addEventListener("click", () => {
   sideBar.classList.toggle("sidebar-visible");
-});
-
-crossBtn.addEventListener("click", () => {
-  sideBar.style.marginLeft = "-400px";
-  sideBar.style.transition = "0.5s";
+  sideBar.classList.toggle("sidebar");
 });
