c55b8316759b43addd77a14fdabaf51475aa841c
Criado Sébastien <dev@letgy.com>
2023-09-20 11:56:58 +0200

    feat: :sparkles: Add page classe with fetch
    Javascript, HTML and CSS

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const sideBar = document.querySelector(".sidebar");
 const button = document.querySelector(".burger");
+const classesLink = document.getElementById("classes");
+
+classesLink.addEventListener("click", () => {
+  // const mainContent = document.querySelector(".main-content");
+
+  // if (mainContent) {
+  //   mainContent.remove();
+  // }
+
+  fetch("classes.html")
+    .then((response) => response.text())
+    .then((content) => {
+      document.querySelector(".content").innerHTML = content;
+    });
+});
 
 button.addEventListener("click", () => {
   sideBar.classList.toggle("sidebar-visible");
