381563df62c233e382cd8edeb2c0211cbb103f47
Criado Sébastien <dev@letgy.com>
2023-09-20 12:31:07 +0200

    feat: :sparkles: Add deploy page with fetch
    HTML,CSS and JS

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const button = document.querySelector(".burger");
 const classesLink = document.getElementById("classes");
 const objectLink = document.getElementById("objects");
 const componentLink = document.getElementById("component");
+const deployLink = document.getElementById("deploy");
 
 classesLink.addEventListener("click", () => {
   fetch("classes.html")
@@ -22,6 +23,14 @@ objectLink.addEventListener("click", () => {
 
 componentLink.addEventListener("click", () => {
   fetch("component.html")
+    .then((response) => response.text())
+    .then((content) => {
+      document.querySelector(".content").innerHTML = content;
+    });
+});
+
+deployLink.addEventListener("click", () => {
+  fetch("deploy.html")
     .then((response) => response.text())
     .then((content) => {
       document.querySelector(".content").innerHTML = content;
